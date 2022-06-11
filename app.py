a2f765cf318c6922978b3dfef4920d762a258cba
Zach Cohn <85762393+zachicecreamcohn@users.noreply.github.com>
2022-06-11 19:41:45 -0400

    trying new things

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import tmdbsimple as tmdb
 import random
 import os
 
+
+print("Starting app...")
 app = Flask(__name__)
 config_object = ConfigParser()
 config_object.read("config.ini")
@@ -40,16 +42,20 @@ token = auth.token_new()
 print(token["expires_at"])
 auth.token_validate_with_login(request_token=token['request_token'],username=config_object["TMDB-LOGIN"]["user"],password=config_object["TMDB-LOGIN"]["pass"])
 if auth.success:
-    print("IT WORKED!")
-    print(token['request_token'])
-    session = auth.session_new(request_token=token['request_token']) # sets up the session
-    session_id = session['session_id']
-    account = tmdb.Account(session_id) # sets up the account associated with the session
-    account.info()
-    list_id = account.lists()['results'][0]['id']
-    movieList = tmdb.Lists(list_id, session_id) # retuns the TMDB list object
-    movieArr = movieList.info()['items'] # this is the array of the movies which is returned by Get Details in API
-    listSize = movieList.info()['item_count'] # this is the number of movies in the array
+    try:
+        print("IT WORKED!")
+        print(token['request_token'])
+        session = auth.session_new(request_token=token['request_token']) # sets up the session
+        session_id = session['session_id']
+        account = tmdb.Account(session_id) # sets up the account associated with the session
+        account.info()
+        list_id = account.lists()['results'][0]['id']
+        movieList = tmdb.Lists(list_id, session_id) # retuns the TMDB list object
+        movieArr = movieList.info()['items'] # this is the array of the movies which is returned by Get Details in API
+        listSize = movieList.info()['item_count'] # this is the number of movies in the array
+    except Exception as e:
+        print(e)
+        pass
 else:
     print("¯\_(ツ)_/¯")
 mysql = MySQL(app)
@@ -148,7 +154,7 @@ def findMovie():
 # run at the start, finds today's movie then passes through all necessary info
 @app.route('/')
 def home():
-    findMovie()
+    # findMovie()
     return render_template('castle.html', title='Cast.le', actor1=actor1, actor2=actor2, actor3=actor3, actor4=actor4, actor5=actor5, director=director, movieTitle=movieTitle,movieDesc=movieDesc, posterPath=posterPath, movieRecommended1=movieRecommended1, movieRecommended2=movieRecommended2, movieRecommended3=movieRecommended3)
 
 
