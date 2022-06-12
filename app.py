034624c58859a36de35f5f56c045b40f5e66ef3b
Zach Cohn <85762393+zachicecreamcohn@users.noreply.github.com>
2022-06-11 20:02:24 -0400

    another attempt

--- app.py
+++ app.py
@@ -216,5 +216,5 @@ def test():
     return "test"
 # run the app
 # app.run(debug=True, port=3456, host='0.0.0.0')
-# port = int(os.getenv('PORT'))
-app.run(debug = False)
+port = int(os.getenv('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
