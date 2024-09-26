a37ca791422ef67d958bd0078c909f6e928b7ed7
nick-cochran <c.nick@wustl.edu>
2024-09-25 22:08:48 -0500

    maybe this will work

--- app.py
+++ app.py
@@ -53,24 +53,26 @@ token = auth.token_new()
 print(token["expires_at"])
 auth.token_validate_with_login(request_token=token['request_token'],username=config_object["TMDB-LOGIN"]["user"],password=config_object["TMDB-LOGIN"]["pass"])
 if auth.success:
-    try:
-        print("IT WORKED!")
-        print(token['request_token'])
-        session = auth.session_new(request_token=token['request_token']) # sets up the session
-        session_id = session['session_id']
-        account = tmdb.Account(session_id) # sets up the account associated with the session
-        account.info()
-        list_id = account.lists()['results'][0]['id']
-        movieList = tmdb.Lists(list_id, session_id) # retuns the TMDB list object
-        print(movieList.info())
-        movieArr = movieList.info()['items'] # this is the array of the movies which is returned by Get Details in API
-        listSize = movieList.info()['item_count'] # this is the number of movies in the array
-    except Exception as e:
-        print(e)
-        print("FUCK IT DIDN'T WORK")
-        pass
+    print("SUCCESS")
 else:
     print("¯\_(ツ)_/¯")
+
+try:
+    print("IT WORKED!")
+    print(token['request_token'])
+    session = auth.session_new(request_token=token['request_token']) # sets up the session
+    session_id = session['session_id']
+    account = tmdb.Account(session_id) # sets up the account associated with the session
+    account.info()
+    list_id = account.lists()['results'][0]['id']
+    movieList = tmdb.Lists(list_id, session_id) # retuns the TMDB list object
+    print(movieList.info())
+    movieArr = movieList.info()['items'] # this is the array of the movies which is returned by Get Details in API
+    listSize = movieList.info()['item_count'] # this is the number of movies in the array
+except Exception as e:
+    print(e)
+    print("FUCK IT DIDN'T WORK")
+    pass
 
 
 mysql = MySQL(app)
