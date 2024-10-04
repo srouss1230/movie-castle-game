38edf440bdba60b7e38a0f98ce3c70d6acd0cc47
srouss1230 <78062025+srouss1230@users.noreply.github.com>
2024-10-04 17:56:52 -0400

    trying print statements but correctly this time

--- app.py
+++ app.py
@@ -165,17 +165,7 @@ def findMovie():
         actor4 = credits['cast'][1]['name']
         actor5 = credits['cast'][0]['name']
 
-        # this finds the tmdb profile paths from all the actors
-        actor1Profile = "https://image.tmdb.org/t/p/w185" + credits['cast'][2]['profile_path']
-        print('******************************')
-        print('******************************')
-        print(actor1Profile)
-        print('******************************')
-        print('******************************')
-        actor2Profile = "https://image.tmdb.org/t/p/w185" + credits['cast'][3]['profile_path']
-        actor3Profile = "https://image.tmdb.org/t/p/w185" + credits['cast'][4]['profile_path']
-        actor4Profile = "https://image.tmdb.org/t/p/w185" + credits['cast'][1]['profile_path']
-        actor5Profile = "https://image.tmdb.org/t/p/w185" + credits['cast'][0]['profile_path']
+       
 
         directorFound = False
         i = 0
@@ -212,6 +202,16 @@ def findMovie():
         movieRecommended2 = row[10]
         movieRecommended3 = row[11]
 
+    print('******************************')
+    print('******************************')
+    print(todaysMovie)
+    print('******************************')
+    print('******************************')
+    print('******************************')
+    print('******************************')
+    print(todaysMovieInfo)
+    print('******************************')
+    print('******************************')
 
 # run at the start, finds today's movie then passes through all necessary info
 @app.route('/')
